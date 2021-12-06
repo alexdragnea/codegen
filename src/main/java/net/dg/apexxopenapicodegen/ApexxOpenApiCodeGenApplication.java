938cafe0af6067ae406eeecde62727cdac9775a2
Alexandru Dragnea <alexdg722@gmail.com>
2021-12-06 17:08:02 +0200

    add

---
+++ src/main/java/net/dg/apexxopenapicodegen/ApexxOpenApiCodeGenApplication.java
@@ -0,0 +1,13 @@
+package net.dg.apexxopenapicodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexxOpenApiCodeGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApexxOpenApiCodeGenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/dg/apexxopenapicodegen/ApexxOpenApiCodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dg.apexxopenapicodegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexxOpenApiCodeGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
